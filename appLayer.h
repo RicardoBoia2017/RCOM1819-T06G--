9691529b4fa334bb17911588352f4bff844f3dd6
RicardoBoia2017 <up201505244@fe.up.pt>
2018-10-24 17:27:37 +0100

    Update appLayer.h

--- appLayer.h
+++ appLayer.h
@@ -3,7 +3,9 @@
 
 //#include "utilities.h"
 #include "linkLayer.h"
-#include "unistd.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 typedef enum {TRANSMITTER, RECEIVER} Status;
 
@@ -11,9 +13,34 @@ typedef struct {
 	Status status;
 } ApplicationLayer;
 
+typedef struct {
+
+	unsigned int type;
+	unsigned char lenght;
+	char * value;
+
+} TLV;
+
+typedef struct {
+
+	unsigned int controlField;
+	TLV* parameters;
+	
+} ControlPacket;
+
+typedef struct {
+
+	unsigned int controlField;
+	unsigned int sequenceNumber;
+	unsigned int nOctets;
+	char * data;
+} DataPacket; 
+
 //void setupAppLayer (ApplicationLayer *appLayer);
 void startAppLayer (LinkLayer *linkLayer, ApplicationLayer * appLayer);
+unsigned int getFileSize (char * fileName);
 void send (LinkLayer * linkLayer);
 void receive (LinkLayer * linkLayer);
+int sendControl(LinkLayer * linkLayer, ControlPacket * controlPacket,int nParameters);
 
 #endif
