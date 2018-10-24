3630747d0fbc9282a5b1dff0cc327cd213db09ed
RicardoBoia2017 <up201505244@fe.up.pt>
2018-10-24 17:56:19 +0100

    Update appLayer.h

--- appLayer.h
+++ appLayer.h
@@ -1,46 +1,48 @@
-#ifndef LINKLAYER_H
-#define LINKLAYER_H
+#ifndef APPLAYER_H
+#define APPLAYER_H
 
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <termios.h>
-#include <strings.h>
-#include <signal.h>
-#include <time.h>
+//#include "utilities.h"
+#include "linkLayer.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
+#define MAX_SIZE 255 //talvez esteja mal
 
+typedef enum {TRANSMITTER, RECEIVER} Status;
 
-#define BAUDRATE B38400
+typedef struct {
+	Status status;
+} ApplicationLayer;
 
 typedef struct {
 
-	int fd; //Descritor de ficheiro
-	char *port; /*Dispositivo /dev/ttySx, x = 0, 1*/
-	int baudRate; /*Velocidade de transmissão*/
-	unsigned int sequenceNumber; /*Número de sequência da trama: 0, 1*/  
-	unsigned int timeout; /*Valor do temporizador: 1 s*/
-	unsigned int numTransmissions;  /*Número de tentativas em caso de falha*/
-	char * frame; //trama
+	unsigned int type;
+	unsigned char lenght;
+	char * value;
+
+} TLV;
+
+typedef struct {
+
+	unsigned int controlField;
+	TLV* parameters;
 	
-	char * fileName; //nome do ficheiro
+} ControlPacket;
 
-	unsigned int nRR;
-	unsigned int nREJ; 
-	double totalTime;
-} LinkLayer;
+typedef struct {
 
-struct termios oldtio, newtio; 
+	unsigned int controlField;
+	unsigned int sequenceNumber;
+	unsigned int nOctets;
+	char * data;
+} DataPacket; 
 
-void setupLinkLayer (LinkLayer *linkLayer);
-void openPort (LinkLayer * linkLayer);
-void setTermiosStructure (LinkLayer * linkLayer);
-void llopenT (LinkLayer * linkLayer); // Tem que retornar inteiro
-void llopenR (LinkLayer * linkLayer); // Tem que retornar inteiro
-int llwrite (LinkLayer *linkLayer, char * buffer, int lenght);
-int llread (LinkLayer * linkLayer, char * buffer);
-void llcloseT (LinkLayer * linkLayer); // Tem que retornar inteiro
-void llcloseR (LinkLayer * linkLayer); // Tem que retornar inteiro
+//void setupAppLayer (ApplicationLayer *appLayer);
+void startAppLayer (LinkLayer *linkLayer, ApplicationLayer * appLayer);
+unsigned int getFileSize (char * fileName);
+void send (LinkLayer * linkLayer);
+void receive (LinkLayer * linkLayer);
+int sendControl(LinkLayer * linkLayer, ControlPacket * controlPacket,int nParameters);
 
 #endif
