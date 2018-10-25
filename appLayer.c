f582272adeb54a1352c776f72270493a1fc5675e
RicardoBoia2017 <up201505244@fe.up.pt>
2018-10-25 18:06:24 +0100

    Update appLayer.c

--- appLayer.c
+++ appLayer.c
@@ -63,13 +63,25 @@ void send (LinkLayer * linkLayer)
 
 	//Data packet
 
-	FILE* file; //Colocar open file
+	FILE* file = fopen(linkLayer->fileName,"rb");; //Mudar isto e colocar função das utilities
 
 	DataPacket dataPacket;
 	dataPacket.data = malloc(fileSize);
 	int nBytesRead = 0;
-	while((nBytesRead = fread(dataPacket.data, sizeof(char), fileSize, file)) > 0)
-		sendData(linkLayer, dataPacket.data, nBytesRead);
+	int sequenceNumber = 0;
+	while((nBytesRead = fread(dataPacket.data, sizeof(char), MAX_SIZE, file)) > 0)
+	{
+//		printf("%d\n", nBytesRead);
+		sendData(linkLayer, dataPacket.data, nBytesRead, sequenceNumber++ % 255);
+
+		memset (dataPacket.data, 0, 255);
+		if (linkLayer->sequenceNumber)
+			linkLayer->sequenceNumber = 0;
+		else
+			linkLayer->sequenceNumber = 1;  
+	}	
+
+	//close file
 
 	//End control packet
 	ControlPacket endCP;
@@ -98,15 +110,16 @@ unsigned int getFileSize (char * fileName)
 
 int sendControl(LinkLayer * linkLayer, ControlPacket * controlPacket,int nParameters)
 {
-	unsigned int packetSize = 1;
-	unsigned int i=0;
+	unsigned int i = 0, packetSize = 1, index = 1;
+
+
 	for(i;i<nParameters;i++)
 		packetSize += 2 + controlPacket->parameters[i].lenght;
 
 	unsigned char frame[packetSize];
 	frame[0] = controlPacket->controlField; //conversão de inteiro para char (pode estar errado)
 	
- 	unsigned int index = 1;
+
 	for (i = 0; i < nParameters; i++)
 	{
 		frame[index++] = controlPacket->parameters[i].type;
@@ -124,13 +137,53 @@ int sendControl(LinkLayer * linkLayer, ControlPacket * controlPacket,int nParame
 	return 0;
 }
 
-int sendData(LinkLayer * linkLayer, char * buffer, int size)
+int sendData(LinkLayer * linkLayer, char * buffer, int size, int sequenceNumber)
 {
+	unsigned char L1, L2, packetSize;
 
+	//usando teorema do resto
+	L1 = size / 256;
+	L2 = size % 256;
+
+	packetSize = 4 + size;
+	unsigned char frame[packetSize];
+
+	frame[0] = 1;
+	frame[1] = sequenceNumber;
+	frame[2] = L2;
+	frame[3] = L1;
+
+	memcpy (&frame[4], buffer, size);
+
+	if (llwrite(linkLayer, frame, packetSize) < 0)
+	{
+		perror("llwrite");
+		exit (-1);
+	}	
 }
 
 
 void receive (LinkLayer * linkLayer)
 {
+	//Start control packet
+	
+	char* buffer = malloc(MAX_SIZE);
+	int size = llread(linkLayer, buffer);
 
+	int index = 0;
+
+	TLV startParameters[2];
+	
+	int j = 1;
+	for(j; j < 2; j++)
+	{
+		startParameters[j].type = frame[index++];
+		startParameters[j].lenght = frame[index++];	
+
+		memcpy(startParameters[j].value, &linkLayer->frame[index], startParameters[j].length);	
+
+		if (startParameters.type == 0) //size
+			fileSize = startParameters[j].value;
+
+	}	
 }
